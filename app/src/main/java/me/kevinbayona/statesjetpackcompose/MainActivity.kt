0b7172c144fdcfa413b475050d415b1d402697ee
Kevin Bayona <119153975+HSkevinYT1@users.noreply.github.com>
2026-09-23 15:07:41 -0500

    maquetacion

--- app/src/main/java/me/kevinbayona/statesjetpackcompose/MainActivity.kt
+++ app/src/main/java/me/kevinbayona/statesjetpackcompose/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import me.kevinbayona.statesjetpackcompose.ui.theme.StatesJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,3 +31,19 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showSystemUi = true)
+@Composable
+fun StateScreen(){
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Contador = 0", fontSize = 32.sp)
+        Spacer(modifier = Modifier.height(25.dp))
+        Button(onClick =  { }) {
+            Text("Sumar 1")
+        }
+    }
+}
